681599925a343c2f4724350c4bc90888f52ee499
TheOneandOnly1999 <86534139+TheOneandOnly1999@users.noreply.github.com>
2022-01-25 03:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
+import pickle
 st.title('Sentiment Analysis')
-import cPickle as pickle
-f = open("big_networkx_graph.pickle","rb")
-model = f.read()
-graph_data = pickle.load(model)
+test_model=pickle.load('model')
 ip=st.text_input('Enter your message')
-op=graph_data.predict([ip])
+op=test_model.predict([ip])
 if st.button('Predict'):
   st.title(op)

9bef1caca546de7093af38d75307bb65c1f93cf7
TheOneandOnly1999 <86534139+TheOneandOnly1999@users.noreply.github.com>
2022-01-25 04:13:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 st.title('Sentiment Analysis')
-test_model=pickle.load('model')
+test_model=pickle.load(open('model.pkl', rb))
 ip=st.text_input('Enter your message')
 op=test_model.predict([ip])
 if st.button('Predict'):

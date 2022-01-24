e8f78ae3c485e205d96ed70dde1cc57a78d8700d
TheOneandOnly1999 <86534139+TheOneandOnly1999@users.noreply.github.com>
2022-01-25 03:18:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-%%writefile app.py
 import streamlit as st
 import pickle
 st.title('Sentiment Analysis')
 test_model=pickle.load('model')
 ip=st.text_input('Enter your message')
 op=test_model.predict([ip])
-if st.button('Predict')
+if st.button('Predict'):
   st.title(op)

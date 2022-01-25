e1803ab2d77c9e9561be58e464086d152d68be79
TheOneandOnly1999 <86534139+TheOneandOnly1999@users.noreply.github.com>
2022-01-25 07:37:25 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pickle
-st.title('Sentiment Analysis')
-test_model= pickle.load(open('model.sav', 'rb'))
-ip=st.text_input('Enter your message')
-op=test_model.predict([ip])
-if st.button('Predict'):
-  st.title(op)
- 
